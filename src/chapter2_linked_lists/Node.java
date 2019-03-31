056f329c5f2db3cc545ac9c5c74b030e3d11e559
Rene Argento <rene.argento@gmail.com>
2019-03-31 17:22:50 +0200

    Added Node class

---
+++ src/chapter2_linked_lists/Node.java
@@ -0,0 +1,15 @@
+package chapter2_linked_lists;
+
+/**
+ * Created by Rene Argento on 24/03/19.
+ */
+public class Node {
+
+    Node next;
+    int value;
+
+    Node(int value) {
+        this.value = value;
+    }
+
+}
