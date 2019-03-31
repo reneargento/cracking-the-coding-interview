5963cda4daa462e1012be15c2a7fae9022b34bfe
Rene Argento <rene.argento@gmail.com>
2019-03-31 17:22:31 +0200

    Added exercise 2.1

---
+++ src/chapter2_linked_lists/Exercise1_RemoveDups.java
@@ -0,0 +1,71 @@
+package chapter2_linked_lists;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Rene Argento on 24/03/19.
+ */
+public class Exercise1_RemoveDups {
+
+    // O(n) runtime
+    // O(n) space
+    public static void removeDupsLinearSpace(Node node) {
+        Set<Integer> valuesSeen = new HashSet<>();
+        Node previous = null;
+
+        while (node != null) {
+            if (valuesSeen.contains(node.value)) {
+                previous.next = node.next;
+            } else {
+                previous = node;
+                valuesSeen.add(node.value);
+            }
+            node = node.next;
+        }
+    }
+
+    // O(n^2) runtime
+    // O(1) space
+    public static void removeDups(Node head) {
+        Node currentNode = head;
+
+        while (currentNode != null) {
+            Node runnerNode = currentNode;
+
+            while (runnerNode.next != null) {
+                if (runnerNode.next.value == currentNode.value) {
+                    runnerNode.next = runnerNode.next.next;
+                } else {
+                    runnerNode = runnerNode.next;
+                }
+            }
+            currentNode = currentNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(2);
+        head.next.next.next.next = new Node(2);
+        head.next.next.next.next.next = new Node(1);
+        head.next.next.next.next.next.next = new Node(4);
+        head.next.next.next.next.next.next.next = new Node(4);
+
+        removeDups(head);
+        System.out.print("List: ");
+        printList(head);
+        System.out.println("\nExpected: 1 2 3 4");
+    }
+
+    private static void printList(Node head) {
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+    }
+}
